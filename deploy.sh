bff848b8cc7ac942dbf8ac39d3b0c7f96eed39c4
lmh <dev@hilberg.eu>
2024-07-26 10:24:10 +0200

    adding COMMIT to env

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 TEX_VERSION=""
-while getopts ":b:v:" opt; do
+TEX_COMMIT=""
+
+while getopts ":b:v:c:" opt; do
   case ${opt} in
     b )
       deploy_branch=$OPTARG
@@ -9,15 +11,19 @@ while getopts ":b:v:" opt; do
     v )
       TEX_VERSION=$OPTARG
       ;;
+    c )
+      TEX_COMMIT=$OPTARG
+      ;;
     \? )
       echo "Invalid option: -$OPTARG" >&2
       ;;
   esac
 done
 
+
 deploy() {
   if [ -n "$deploy_branch" ]; then
-    mike deploy --branch ${deploy_branch} --update-aliases --push ${TEX_VERSION} ${REPO_COMMIT} latest || mike deploy --branch ${deploy_branch} --push ${TEX_VERSION} ${REPO_COMMIT} latest
+    mike deploy --branch ${deploy_branch} --update-aliases --push ${TEX_VERSION} ${TEX_COMMIT} latest || mike deploy --branch ${deploy_branch} --push ${TEX_VERSION} ${TEX_COMMIT} latest
     mike set-default --branch ${deploy_branch} --push latest
   else
     mkdocs serve
@@ -25,7 +31,6 @@ deploy() {
 }
 
 export build_doc_date=$(date -u '+%d.%m.%Y')
-export REPO_COMMIT=$(git log -1 --pretty=format:%H)
 
 deploy
 
